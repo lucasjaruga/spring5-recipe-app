234a5fe0998f6ebb4abc4dbbd83cf0f6bbf85b0c
Lucas Jaruga <lucas.jaruga@outlook.com>
2020-09-07 15:47:49 +0200

    added Category POJO and set up a relationship between that and Recipe

---
+++ src/main/java/pl/jarugalucas/spring5recipeapp/model/Category.java
@@ -0,0 +1,41 @@
+package pl.jarugalucas.spring5recipeapp.model;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Recipe> getRecipeSet() {
+        return recipes;
+    }
+
+    public void setRecipeSet(Set<Recipe> recipe) {
+        this.recipes = recipe;
+    }
+}
