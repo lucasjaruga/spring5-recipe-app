1fedaf96f353678ac281d611adb2847a1c724624
Lucas Jaruga <lucas.jaruga@outlook.com>
2020-09-07 13:05:25 +0200

    initialized project with spring initializer

---
+++ src/test/java/pl/jarugalucas/spring5recipeapp/Spring5RecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jarugalucas.spring5recipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5RecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
