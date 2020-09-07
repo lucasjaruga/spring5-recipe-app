d2742d8a85ee9c6108488e5a995d20b79d7988f6
Lucas Jaruga <lucas.jaruga@outlook.com>
2020-09-07 15:09:34 +0200

    added Difficulty lvl

---
+++ src/main/java/pl/jarugalucas/spring5recipeapp/model/Difficulty.java
@@ -0,0 +1,5 @@
+package pl.jarugalucas.spring5recipeapp.model;
+
+public enum Difficulty {
+    EASY, MODERATE, HARD, SUPER_HARD
+}
--- src/main/java/pl/jarugalucas/spring5recipeapp/model/Recipe.java
+++ src/main/java/pl/jarugalucas/spring5recipeapp/model/Recipe.java
@@ -17,8 +17,9 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    //todo add
-    //private Difficulty difficulty;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     // mappedBy -> target property in Ingredient class
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
@@ -93,6 +94,14 @@ public class Recipe {
 
     public void setDirections(String directions) {
         this.directions = directions;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 
     public Set<Ingredient> getIngredients() {
