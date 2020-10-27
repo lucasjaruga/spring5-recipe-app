369b7591e4157eb37481a30e15450dd555039346
Lucas Jaruga <lucas.jaruga@outlook.com>
2020-10-27 16:17:08 +0100

    added recipeform.html and modified RecipeController for adding new recipes

--- src/main/java/pl/jarugalucas/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/pl/jarugalucas/spring5recipeapp/controllers/RecipeController.java
@@ -2,8 +2,11 @@ package pl.jarugalucas.spring5recipeapp.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.jarugalucas.spring5recipeapp.commands.RecipeCommand;
 import pl.jarugalucas.spring5recipeapp.services.RecipeService;
 
 @Controller
@@ -21,5 +24,20 @@ public class RecipeController {
         model.addAttribute("recipe", recipeService.findById(new Long(id)));
 
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
